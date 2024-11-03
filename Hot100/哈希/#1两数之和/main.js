ccccd7fa20646d4fef9a7e8e13a9556648cf95c6
Jackson <JacksonHe04@outlook.com>
2024-11-04 00:59:19 +0800

    1. 两数之和（数组，哈希表）

--- Hot100/哈希/#1两数之和/main.js
+++ Hot100/哈希/#1两数之和/main.js
@@ -3,6 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+const twoSum = function (nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    console.log("complement=", complement);
+    if (map[complement] !== undefined) {
+      return [map[complement], i];
+    }
+    map[nums[i]] = i;
+    console.log("i=", i, map);
+  }
 };
+
+function main() {
+  console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+  console.log(twoSum([3, 3], 6)); // Output: [0, 1]
+}
+
+main();
